3b0cdc82663218bffe0e167b69e70533afdb51c3
D&D <D&D@DESKTOP-TDG3MFR.attlocal.net>
2021-08-05 22:09:16 -0400

    Added additional data methods

--- src/com/dbms/DBConnect.java
+++ src/com/dbms/DBConnect.java
@@ -4,9 +4,11 @@ import java.sql.*;
 
 public class DBConnect
 {
-    public DBConnect()
-    {
+    private Connection Conn; // This will be our class variable to store the database conneciton
+    
+    public DBConnect() {
         //try to connect to the database
+        Conn = null; // We'll first set the connection to null so we can verify it's conenction later
         try {
             //drivermanager
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,27 +22,23 @@ public class DBConnect
             String password = "14708690";
 
             //Connection
-            try (Connection conn = DriverManager.getConnection(url, username, password))
+            try
             {
-            //statement
+                Conn = DriverManager.getConnection(url, username, password);
+                
+                //statement
                 System.out.println("Connection Successful!");
-
-                String sql = "SELECT * FROM employee";
-
-                Statement statement = conn.createStatement();
-                ResultSet result = statement.executeQuery(sql);
-
                 int count = 0;
-
+                ResultSet result = SqlSelectAll("SELECT * FROM employee"); // Use our class method to run the query for us.
                 while (result.next()){
                     String emplID = result.getString("empl_id");
                     String fName = result.getString("fname");
                     String lName = result.getString("lname");
                     String access = result.getString("access");
-
                     String output = "User #%d: %s - %s - %s - %s";
                     System.out.println(String.format(output, ++count, emplID, fName, lName, access));
                 }
+                result.close(); // Always make sure to close your result sets
 
             } catch (SQLException ex) {
                 ex.printStackTrace();
@@ -51,5 +49,102 @@ public class DBConnect
             e.printStackTrace();
             System.out.println("Could not connect to database");
         }
+    }
+    
+    // Below are the methods to manage the database functions
+    public void Dispose() {
+        try {
+            Conn.close(); // We're closing the connection to ensure that there are no orphaned connections to our database
+        }
+        catch (SQLException err) { 
+            err.printStackTrace();
+            System.out.println("Error: " + err.getMessage());
+        }
+    }
+    public void SqlInsert(String table, String columns, String values) {
+        // Example use DBConnect.SqlInsert("employee", "empl_id, fname, lname, access, phone, username, password", "'0001', 'John', 'Doe', '1', '123-456-7890', 'jdoe', 'MySuperSecretPwd'");
+        try {
+            // Run the SQL insert statment based on the table data provided
+            Statement SqlStmt = Conn.createStatement();
+            SqlStmt.executeUpdate("INSERT INTO " + table + " (" + columns + ") VALUES (" + Clean(values) + ")");
+            System.out.println("Insert completed successfully!");
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            System.out.println("An error occured when inserting data into " + table + "." +
+                               "\nPlease review the error below for the cause of the exception.\nError: " + err.getMessage());
+        }
+    }
+    public void SqlDelete(String table, String where) {
+        // Example use DBConnect.SqlDelete("employee", "empl_id = '0002'");
+        try {
+            // Delete all the data from the table where the values equal what's in the where statement
+            Statement SqlStmt = Conn.createStatement();
+            SqlStmt.executeUpdate("DELETE FROM " + table + " WHERE " + Clean(where));
+            System.out.println("Delete compeleted successfully!");
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            System.out.println("An error occured when deleting data from " + table + "." +
+                               "\nPlease review the error below for the cause of the exception.\nError: " + err.getMessage());
+        }
+    }
+    public void SqlUpdate(String table, String set, String where) {
+        // Example use DBConnect.SqlUpdate("employee", "fname = 'Jon'", "emlp_id = '0002'");
+        try {
+            // Update the table based on the values provided
+            Statement SqlStmt = Conn.createStatement();
+            SqlStmt.executeUpdate("UPDATE " + table + " SET " + Clean(set) + " WHERE " + Clean(where));
+            System.out.println("Update compeleted successfully!");
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            System.out.println("An error occured when updating data inside " + table + "." +
+                               "\nPlease review the error below for the cause of the exception.\nError: " + err.getMessage());
+        }
+    }
+    
+    public ResultSet SqlSelectAll(String query) {
+        // Example use DBConnect.SqlSelectAll("SELECT * FROM employee");
+        ResultSet rs = null; // We'll make the default ResultSet null so our other can code can validate whether there is a query returned
+        try {
+            // Retrieve the ResultSet from the given query
+            Statement sqlStmt = Conn.createStatement();
+            rs = sqlStmt.executeQuery(Clean(query));
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            System.out.println("An error occured when executing the query '" + query + "'.\nPlease review the error below for the cause of the exception." +
+                               "\nError: " + err.getMessage());
+        }
+        return rs;
+    }
+    public String SqlSelectSingle(String query) {
+        // Example use DBConnect.SqlSelectSingle("SELECT fname + ' ' + lname FROM employee WHERE empl_id = '0002'");
+        String val = ""; // We will return a blank String if the query fails.
+        try {
+            Statement sqlStmt = Conn.createStatement();
+            ResultSet rs = sqlStmt.executeQuery(Clean(query));
+            if (rs.next()) // We only want the first value of the first column if there is data returned
+                val = rs.getString(1); // We want to convert the returned database value to a string even if it's not. We can convert the value back later.
+            rs.close(); // Remember to close your ResultSets
+            sqlStmt.close(); // And your SQL commands
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+            System.out.println("An error occured when executing the query '" + query + "'.\nPlease review the error below for the cause of the exception." +
+                               "\nError: " + err.getMessage());
+        }
+        return val;
+    }
+    public Boolean DBReadyForUse() {
+        if (Conn != null)
+            return true;
+        return false; // Note: we do not need an else statment becuase this code will not run anyways if the above if statement is false
+    }
+    
+    // Below are the private methods to the class that will be used to validate the user's input
+    private String Clean(String original) {
+        return original.replace(";", ""); // Note: we use String.replace(String, String) instead of chars because an error is thrown when replacing a char with a blank.
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,6 +3,27 @@ import com.dbms.DBConnect;
 public class main {
     public static void main(String[] args) {
         DBConnect db = new DBConnect();
-
+        
+        if (db.DBReadyForUse()) 
+        {
+            // Run some test queries
+            System.out.println();
+            System.out.println("The first user is: " + db.SqlSelectSingle("SELECT concat(fname, ' ', lname) FROM employee WHERE empl_id = '0001'"));
+            
+            System.out.println();
+            db.SqlInsert("employee", "empl_id, fname, lname, access, phone, username, password", "'9999', 'Tester', 'McTest', '1', 'No Phone', 'T-Act', 'NotSecure123'");
+            System.out.println("Newly created user is: " + db.SqlSelectSingle("SELECT concat(fname, ' ', lname) FROM employee WHERE empl_id = '9999'"));
+            
+            System.out.println();
+            db.SqlUpdate("employee", "password = 'This_is_b3tt3r'", "empl_id = '9999'");
+            System.out.println("The updated password is: " + db.SqlSelectSingle("SELECT password FROM employee WHERE empl_id = '9999'"));
+            
+            System.out.println();
+            db.SqlDelete("employee", "empl_id = '9999'");
+            if (db.SqlSelectSingle("SELECT concat(fname, ' ', lname) FROM employee WHERE empl_id = '9999'").equals(""))
+                System.out.println("The test employee no longer exists");
+            else
+                System.out.println("This method failed");
+        }
     }
 }
