d05fdc35e71f6385f377b8d79686caee4f64e420
JesseBrewer22 <jaybrewer22@icloud.com>
2021-08-14 18:16:31 -0500

    Please check over code and let me know what else I should add.

--- EquipmentCheckoutSystem/src/ims/EquipmentManager.java
+++ EquipmentCheckoutSystem/src/ims/EquipmentManager.java
@@ -6,47 +6,59 @@ import java.util.Objects;
 
 import dbms.EmployeeDB;
 
+import dbms.InventoryDB;
+
+import dbms.DBConnect;
+
+import dbms.EquipmentRequest;
+
+import dbms.OrderDetail;
+
+
+
 public class EquipmentManager {
+
+    private static String username;
     public static void main(String[] args) {
     }
     private String fName;
     private String lName;
-    private String passwordVerified;
-    private int id;
+    private String password;
+    private int employ_id;
     private int verifyEquipManager;
     private int searchDatabase;
     
 // This will grab the Employee Information
-    public EquipmentManager(String fName, String lName, int id, String passwordVerified) {
+    public EquipmentManager(String fName, String lName, int employ_id, String password) {
        this.fName = fName;
        this.lName = lName;
-       this.id =id;
-       this.passwordVerified = passwordVerified;
+       this.employ_id =employ_id;
+       this.password = password;
        System.out.println("You are verified.");
     }
-    public String getfName() {
+    public String getfirstName() {
         return fName;
     }
-    public void setfName(String fName) {
+    public void setfirstName(String fName) {
         this.fName = fName;
     }
     public String getlName() {
         return lName;
     }
     public void setlName(String lName) {
-        this.lName = fName;
+        this.lName = lName;
     }
-    public int getId() {
-        return id;
+    public int getemploy_Id() {
+        return employ_id;
     }
-    public void setId(int id) {
-        this.id = id;
+    public void setemploy_Id(int employ_id) {
+        this.employ_id = employ_id;
     }
-    public String getPasswordVerifed() {
-        return passwordVerified;
+    public String get() {
+        return password;
     }
-    public void setPasswordVerified(String passwordVerifed) {
-        this.passwordVerified = passwordVerifed;
+    public void setPassword(String password) {
+        this.password = password;
     }
     public int getVerifyEquipMngr() {
         return verifyEquipManager;
@@ -61,15 +73,23 @@ public class EquipmentManager {
         this.searchDatabase = searchDatabase;
     }
     public Boolean Verify(){
-        Boolean login = false;
+        Boolean login = true;
         return login;
     }
     public void verifyEqupiManager(){
     }
-   /* private void searchDataBase(){
+    private void searchDataBase(){
         System.out.println("Your order is on the way.");
-        
-        
-        EmployeeDB.search(fName, lName, id, passwordVerified);
-    }*/
-}
+    }
+    public static ResultSet search(String fName, String lName, String employ_id, String password, DBConnect db) {
+        ResultSet rs = (ResultSet) db.SqlSelectAll("SELECT * FROM employee WHERE empl_id LIKE '%" + employ_id + "%' OR lname LIKE '%" + lName + "%' OR username LIKE '%" + username + "%' OR password LIKE '%" + password + "%'");
+        return rs;
+    }
+
+    private static class ResultSet {
+
+        public ResultSet() {
+        } 
+    }
+    
+    }
