431a76bdfff68066b066fd9130aa1761ef380984
Zmurray22 <88351080+Zmurray22@users.noreply.github.com>
2021-08-13 20:32:17 -0500

    Working on MaintEmployee

--- EquipmentCheckoutSystem/src/ims/MaintEmployee.java
+++ EquipmentCheckoutSystem/src/ims/MaintEmployee.java
@@ -1,4 +1,54 @@
 package ims;
 
 public class MaintEmployee {
+    private String fname;
+    private String lname;
+    private String id;
+    private String password;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Boolean Verify(){
+        Boolean login = false;
+        
+        return login;
+    }
+    
+    public void CheckAccount(){
+        
+    }
+    
+    private void GetEquip(){
+        
+    }
 }
