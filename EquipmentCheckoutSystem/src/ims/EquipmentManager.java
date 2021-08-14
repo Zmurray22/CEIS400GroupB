e83f1256d47a7272713bb2b5bd0802747b6cfa0d
JesseBrewer22 <jaybrewer22@icloud.com>
2021-08-14 12:52:32 -0500

    I will be working on this code tonight

--- EquipmentCheckoutSystem/src/ims/EquipmentManager.java
+++ EquipmentCheckoutSystem/src/ims/EquipmentManager.java
@@ -1,67 +1,75 @@
 package ims;
 
-
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Objects;
 
-public class EquipmentManager { 
+import dbms.EmployeeDB;
 
+public class EquipmentManager {
+    public static void main(String[] args) {
+    }
     private String fName;
     private String lName;
     private String passwordVerified;
     private int id;
     private int verifyEquipManager;
     private int searchDatabase;
-
+    
 // This will grab the Employee Information
     public EquipmentManager(String fName, String lName, int id, String passwordVerified) {
        this.fName = fName;
        this.lName = lName;
        this.id =id;
        this.passwordVerified = passwordVerified;
-        System.out.println("Employee is verified");
+       System.out.println("You are verified.");
     }
-
-    public String getfName() { return fName; }
-
-    public void setfName(String fName) { this.fName = fName; }
-    
-    public String getlName() { return lName; }
-    
-    public void setlName(String lName) { this.lName = fName; }
-    
-    public int getId() { return id; }
-
-    public void setId(int id) { this.id = id; }
-
-    public String getPasswordVerifed() { return passwordVerified; }
-
-    public void setPassword(String passwordVerifed) {
+    public String getfName() {
+        return fName;
+    }
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+    public String getlName() {
+        return lName;
+    }
+    public void setlName(String lName) {
+        this.lName = fName;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPasswordVerifed() {
+        return passwordVerified;
+    }
+    public void setPasswordVerified(String passwordVerifed) {
         this.passwordVerified = passwordVerifed;
     }
-
-    public int getVerifyEquipMngr() { return verifyEquipManager; }
-
+    public int getVerifyEquipMngr() {
+        return verifyEquipManager;
+    }
     public void setVerifyEquipMngr(int verifyEquipManager) {
         this.verifyEquipManager = verifyEquipManager;
     }
-
-    public int getSearchDatabase() { return searchDatabase; }
-
+    public int getSearchDatabase() {
+        return searchDatabase;
+    }
     public void setSearchDatabase(int searchDatabase) {
         this.searchDatabase = searchDatabase;
     }
-
-    @Override
-    public String toString() {
-        return "EquipmentManager{" +
-                "fName='" + fName + '\'' +
-                ", lName='" + lName + '\'' +
-                ", password='" + passwordVerified + '\'' +
-                ", id=" + id +
-                ", verifyEquipManager=" + verifyEquipManager +
-                ", searchDatabase=" + searchDatabase +
-                '}';
+    public Boolean Verify(){
+        Boolean login = false;
+        return login;
+    }
+    public void verifyEqupiManager(){
+    }
+    private void searchDataBase(){
+        System.out.println("Your order is on the way.");
+        
+        
+        EmployeeDB.search(fName, lName, id, passwordVerified);
     }
 }
