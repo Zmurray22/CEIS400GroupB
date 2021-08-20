219255440249602987004da1255adce98494a8f2
Zmurray22 <88351080+Zmurray22@users.noreply.github.com>
2021-08-20 11:29:06 -0500

    Added test driver for Account

--- EquipmentCheckoutSystem/src/testDrivers/AccountTest.java
+++ EquipmentCheckoutSystem/src/testDrivers/AccountTest.java
@@ -5,10 +5,23 @@
  */
 package testDrivers;
 
-/**
- *
- * @author Zach
- */
+import dbms.Account;
+import java.sql.SQLException;
+import java.text.ParseException;
+
 public class AccountTest {
-    
+    public static void main(String[] args) throws ParseException, SQLException{
+        String table = "";
+        String username = "";
+        String equip_id = "";
+        String qty = "";
+        
+        
+        Account.checkExists(table);
+        Account.createAcc(table);
+        Account.search(table);
+        Account.showAccount(username);
+        Account.update(equip_id, qty);
+        Account.userProfile(username);
+    }
 }
