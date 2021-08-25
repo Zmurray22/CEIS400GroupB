6f62ebf6553f3731cd2c187f8ddb03b398f1472c
Zmurray22 <88351080+Zmurray22@users.noreply.github.com>
2021-08-25 14:13:45 -0500

    a

--- EquipmentCheckoutSystem/src/dbms/EquipmentRequest.java
+++ EquipmentCheckoutSystem/src/dbms/EquipmentRequest.java
@@ -11,13 +11,22 @@ public class EquipmentRequest {
     private ArrayList<String> order = new ArrayList<String>();
     private ArrayList<String> cart = new ArrayList<String>();
     private String userID;
-    private String fname;
-    private String lname;
-    private String access;
+    private String userName;
 
     public EquipmentRequest() {
     }
-   
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    private String fname;
+    private String lname;
+    private String access;
+    
     public String getFname() {
         return fname;
     }
@@ -78,6 +87,6 @@ public class EquipmentRequest {
     public void sendOrder(ArrayList<String> sentOrder) throws ParseException, SQLException{
         String[] orderArr = sentOrder.toArray(new String[0]);
         System.out.println(Arrays.toString(orderArr));
-        Account.update(getCurrentUser(), orderArr);
+        Account.update(getUserName(), orderArr);
     }
 }
